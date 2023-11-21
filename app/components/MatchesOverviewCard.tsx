377a1556cc98c8b63e631210d2540a26646c91de
Piero Coronado <pacoronado01@gmail.com>
2023-11-21 12:35:33 -0500

    Display content based on size of matches array

--- app/components/MatchesOverviewCard.tsx
+++ app/components/MatchesOverviewCard.tsx
@@ -4,25 +4,32 @@ export default function MatchesOverviewCard({
 	matches,
 }: {
 	leagueName: string;
-	matches: Match[] | null;
+	matches: Match[];
 }) {
-	// console.log(matches);
+	let content =
+		matches.length === 0 ? (
+			<div>
+				<p>No Matches Today</p>
+			</div>
+		) : (
+			matches.map((match) => {
+				const { time } = convertDateToLocalTime(match.fixture.date);
+				return (
+					<div key={match.fixture.id}>
+						<p>
+							{match.teams.home.name} vs {match.teams.away.name}
+						</p>
+						<p>{time}</p>
+					</div>
+				);
+			})
+		);
+
 	return (
 		<div className="bg-gradient-to-t from-lime-100 to-lime-500 p-1 rounded-md shadow-lg">
 			<h2 className="text-3xl font-semibold text-white">{leagueName}</h2>
-			<div className="flex flex-col bg-green-500 px-1 min-h-[100px] min-w-200px]">
-				{matches &&
-					matches.map((match) => {
-						const { time } = convertDateToLocalTime(match.fixture.date);
-						return (
-							<div key={match.fixture.id}>
-								<p>
-									{match.teams.home.name} vs {match.teams.away.name}
-								</p>
-								<p>{time}</p>
-							</div>
-						);
-					})}
+			<div className="flex flex-col bg-green-500 px-1 min-h-[100px] min-w-[250px]">
+				{content}
 			</div>
 		</div>
 	);
