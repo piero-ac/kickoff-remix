9e878f5309ad1891396d46a36e9b1975842ac76e
Piero Coronado <pacoronado01@gmail.com>
2023-11-21 12:32:07 -0500

    Create function for obtaining today's matches

---
+++ utils/api-football-functions.tsx
@@ -0,0 +1,38 @@
+import { json } from "@remix-run/node";
+
+const headers = new Headers();
+headers.append("X-RapidAPI-Key", process.env.RAPIDAPI_API_KEY as string);
+headers.append("X-RapidAPI-Host", process.env.RAPIDAPI_HOST as string);
+
+const options: RequestInit = {
+	method: "GET",
+	headers: headers,
+};
+
+export async function getTodaysMatches(
+	leagueId: number,
+	season: string,
+	date: string
+) {
+	try {
+		const response = await fetch(
+			`https://api-football-v1.p.rapidapi.com/v3/fixtures?date=${date}&league=${leagueId}&season=${season}`,
+			options
+		);
+		if (!response.ok) {
+			throw json({ message: "Could not fetch fixtures" }, { status: 500 });
+		}
+
+		const result = await response.json();
+		const matches = await result.response;
+		return matches;
+	} catch (error: any) {
+		if (error instanceof Error)
+			throw json(
+				{
+					message: error.message,
+				},
+				{ status: 500 }
+			);
+	}
+}
