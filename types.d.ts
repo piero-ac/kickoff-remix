6d15513563fbf98fbeb66530c617c1998d1be54f
Piero Coronado <pacoronado01@gmail.com>
2023-11-24 16:42:16 -0500

    Create type for standings array

--- types.d.ts
+++ types.d.ts
@@ -104,3 +104,49 @@ type Season = {
 		odds: boolean;
 	};
 };
+
+type Standings = {
+	rank: number;
+	team: {
+		id: number;
+		name: string;
+		logo: string | null;
+	};
+	points: number;
+	goalsDiff: number;
+	group: string;
+	form: string;
+	status: string;
+	description: string;
+	all: {
+		played: number;
+		win: number;
+		draw: number;
+		lose: number;
+		goals: {
+			for: number;
+			against: number;
+		};
+	};
+	home: {
+		played: number;
+		win: number;
+		draw: number;
+		lose: number;
+		goals: {
+			for: number;
+			against: number;
+		};
+	};
+	away: {
+		played: number;
+		win: number;
+		draw: number;
+		lose: number;
+		goals: {
+			for: number;
+			against: number;
+		};
+	};
+	update: string;
+};
