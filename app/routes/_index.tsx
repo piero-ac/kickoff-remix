a1bfb235a29de3c75c101baed59192783ffe9694
Piero Coronado <pacoronado01@gmail.com>
2024-01-03 13:24:51 -0500

    Change landing page to use custom colors

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,9 +1,11 @@
 import { type MetaFunction } from "@remix-run/node";
 import { Suspense } from "react";
-import { Await, useLoaderData } from "@remix-run/react";
-import { defer } from "@remix-run/node";
+import { Await, useLoaderData, useRouteError } from "@remix-run/react";
+import { defer, json } from "@remix-run/node";
 import getTodaysMatches from "~/api/getTodaysMatches";
+import getLeagueStandings from "~/api/getLeagueStandings";
 import { getDate } from "utils/datetime-functions";
+import Standings from "~/components/Standings";
 import MatchesOverviewTable from "~/components/MatchesOverviewTable";
 
 export const meta: MetaFunction = () => {
@@ -18,50 +20,47 @@ export const meta: MetaFunction = () => {
 
 export const loader = async () => {
 	const today = getDate();
-	const premMatches: Promise<Match[]> = getTodaysMatches(39, "2023", today);
-
-	return defer({ premMatches });
+	const premMatches: Promise<Match[]> = await getTodaysMatches(
+		39,
+		"2023",
+		today
+	);
+	const leagueStandings: Standings = await getLeagueStandings("39", "2023");
+	return json({ premMatches, leagueStandings });
 };
 
 export default function Index() {
-	const { premMatches } = useLoaderData<typeof loader>();
+	const { premMatches, leagueStandings } = useLoaderData<typeof loader>();
 
 	return (
-		<div className="relative h-[sm-height] md:h-[md-height]">
-			{/* Background Image */}
-			<div className="absolute inset-0 z-0">
-				<div className="bg-[url('/img/soccer-pitch-sky.jpg')] bg-cover bg-center h-full"></div>
-			</div>
-			{/* Content */}
-			<div className="absolute inset-0 z-20 mt-14 px-3">
-				<div className="text-white flex flex-col text-center items-center">
-					<h1 className="hidden md:block text-lime-500 font-bold text-9xl italic mb-5">
-						Kick⚽ff
-					</h1>
-					<p className="text-3xl sm:text-4xl sm:w-[400px] md:text-5xl font-semibold text-lime-100 md:w-[500px]">
-						View <span className="green-gradient ">Matches</span>,
-						<span className="green-gradient"> Scores</span>, and
-						<span className="green-gradient"> Statistics</span>!
-					</p>
-					<div className="mt-3 md:mt-5 text-4xl md:text-6xl green-gradient">
-						&#x21e3;
-					</div>
-					<div className="mt-3 flex flex-col border border-black w-full mx-6 md:max-w-[900px]">
-						<h2 className="bg-stone-900">Current Matches</h2>
-						<div className="bg-lime-200 text-black max-h-[300px] overflow-auto">
-							<Suspense fallback={<p>Loading...</p>}>
-								<Await resolve={premMatches}>
-									{(resolvedMatches) => (
-										<MatchesOverviewTable matches={resolvedMatches} />
-									)}
-								</Await>
-							</Suspense>
-						</div>
-					</div>
-				</div>
-			</div>
-			{/* Overlay */}
-			<div className="absolute inset-0 bg-green-600 opacity-50 z-10"></div>
+		<div>
+			{/* Header */}
+			<header className="mt-5 mb-5 h-[200px] cyan-gradient flex justify-center items-center">
+				<h1 className=" text-brightwhite font-bold text-7xl">Premier League</h1>
+			</header>
+			<section className="flex flex-col sm:flex-row items-center justify-center max-w-5xl mx-auto gap-4 px-1">
+				{/* League Table */}
+				<section className="rounded-lg w-[300px] md:w-[400px]">
+					<h2 className="cyan-gradient text-brightwhite font-bold text-center text-3xl py-1 rounded-t-lg w-[300px] md:w-[400px]">
+						League Standings
+					</h2>
+					<Standings standings={leagueStandings.standings} />
+				</section>
+				{/* Live Matches */}
+				<aside className="border grow">
+					<h3>Matches</h3>
+				</aside>
+			</section>
+		</div>
+	);
+}
+
+export function ErrorBoundary() {
+	const error = useRouteError() as Error;
+	return (
+		<div>
+			<h1>Something went wrong!</h1>
+			{error.message}
 		</div>
 	);
 }
