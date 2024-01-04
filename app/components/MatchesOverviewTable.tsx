dd84a1deebcf12bd417210d2999b180543456c71
Piero Coronado <pacoronado01@gmail.com>
2024-01-03 19:26:15 -0500

    Add styling to table

--- app/components/MatchesOverviewTable.tsx
+++ app/components/MatchesOverviewTable.tsx
@@ -1,29 +1,34 @@
 import Match from "./Match";
+import { convertDateToLocalTime } from "utils/datetime-functions";
 
 export default function MatchesOverviewTable({
 	matches,
 }: {
 	matches: Match[];
 }) {
-	if (matches.length === 0) return null;
+	let content =
+		matches.length === 0 ? (
+			<div className="bg-brightwhite">No Matches Today</div>
+		) : (
+			matches.map((match) => {
+				const { time } = convertDateToLocalTime(match.fixture.date);
+				return (
+					<div key={match.fixture.id}>
+						<p>
+							{match.teams.home.name} vs {match.teams.away.name}
+						</p>
+						<p>{time}</p>
+					</div>
+				);
+			})
+		);
 
-	const league = matches[0].league.id;
-	let src = "";
-	switch (league) {
-		case 39:
-			src = "/img/premierleague-logo.png";
-			break;
-		case 140:
-			src = "/img/laliga-logo.png";
-			break;
-		case 78:
-			src = "/img/bundesliga-logo.png";
-			break;
-		default:
-			src = "/img/website-icon.png";
-	}
-
-	return matches.map((match: Match) => (
-		<Match key={match.fixture.id} match={match} src={src} />
-	));
+	return (
+		<div className="w-full h-auto">
+			<h2 className="cyan-gradient text-brightwhite font-bold text-center text-3xl py-1 rounded-t-lg">
+				{matches.length === 0 ? "Upcoming Matches" : "Today's Matches"}
+			</h2>
+			<div className="flex flex-col">{content}</div>
+		</div>
+	);
 }
