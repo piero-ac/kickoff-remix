64fd9694aa5137bd9d7bae3af8158a8dcd879e39
Piero Coronado <pacoronado01@gmail.com>
2024-01-04 15:56:09 -0500

    Refactor loader function

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -5,7 +5,7 @@ import { defer, json } from "@remix-run/node";
 import getTodaysMatches from "~/api/getTodaysMatches";
 import getLeagueStandings from "~/api/getLeagueStandings";
 import getMatchdates from "~/api/getMatchdates";
-import { getDate } from "utils/datetime-functions";
+import { getDate, getNextMatchdate } from "utils/datetime-functions";
 import Standings from "~/components/Standings";
 import MatchesOverviewTable from "~/components/MatchesOverviewTable";
 
@@ -21,29 +21,57 @@ export const meta: MetaFunction = () => {
 
 export const loader = async () => {
 	const today = getDate();
-	// const matchdates = await getMatchdates(39, "2023");
-	const premMatches: Match[] = await getTodaysMatches(39, "2023", today);
+	const matchdates = await getMatchdates(39, "2023");
+	const nextMatchdate = getNextMatchdate(matchdates, today);
 	const leagueStandings: Standings = await getLeagueStandings("39", "2023");
-	return json({ premMatches, leagueStandings });
+	let matches: {
+		premMatches: Match[];
+		seasonComplete: boolean;
+		today: string;
+		nextMatchdate: string;
+	} = {
+		premMatches: [],
+		seasonComplete: false,
+		today,
+		nextMatchdate,
+	};
+	if (nextMatchdate !== "Season Completed") {
+		matches.premMatches = await getTodaysMatches(39, "2023", nextMatchdate);
+	} else {
+		matches.seasonComplete = true;
+	}
+	return json({ matches, leagueStandings });
 };
 
 export default function Index() {
-	const { premMatches, leagueStandings } = useLoaderData<typeof loader>();
+	const { matches, leagueStandings } = useLoaderData<typeof loader>();
 
 	return (
-		<div>
+		<div className="my-1">
 			{/* Header */}
-			<header className="mt-5 mb-5 h-[200px] cyan-gradient flex justify-center items-center">
-				<h1 className=" text-brightwhite font-bold text-7xl">Premier League</h1>
+			<header className="mt-5 mb-5 h-[150px] md:h-[200px] cyan-gradient flex flex-col sm:flex-row justify-center items-center">
+				<h1 className=" text-brightwhite font-bold text-5xl md:text-7xl ">
+					Premier League
+				</h1>
+				<img
+					src="/img/premierleague-logo.png"
+					className="h-[50px] w-[50px] md:h-[120px] md:w-[120px]"
+					alt="Premier League Logo"
+				/>
 			</header>
-			<section className="flex flex-col sm:flex-row items-center justify-center max-w-5xl mx-auto gap-4 px-1">
+			<section className="flex flex-col-reverse sm:flex-row items-center justify-center max-w-5xl mx-auto gap-2 lg:gap-4 px-1">
 				{/* League Table */}
 				<section className="rounded-lg w-[300px] md:w-[400px]">
 					<Standings standings={leagueStandings.standings} />
 				</section>
 				{/* Live Matches */}
-				<aside className="border grow rounded-lg">
-					<MatchesOverviewTable matches={premMatches} />
+				<aside className="rounded-lg sm:self-start">
+					<MatchesOverviewTable
+						matches={matches.premMatches}
+						seasonComplete={matches.seasonComplete}
+						today={matches.today}
+						nextMatchdate={matches.nextMatchdate}
+					/>
 				</aside>
 			</section>
 		</div>
