10c6977fc91d0a6debf5ba4454e9490725e4e7c4
Piero Coronado <pacoronado01@gmail.com>
2024-01-06 11:55:29 -0500

    Create component for match filter options

---
+++ app/components/MatchSelectButton.tsx
@@ -0,0 +1,20 @@
+export default function MatchSelectButton({
+	text,
+	selected,
+	onClick,
+}: {
+	text: string;
+	selected: boolean;
+	onClick: () => void;
+}) {
+	return (
+		<button
+			className={`text-brightwhite p-1 w-[100px] hover:bg-hotpink hover:font-semibold ${
+				!selected ? "bg-darkpurple" : "bg-hotpink font-semibold"
+			} `}
+			onClick={onClick}
+		>
+			{text}
+		</button>
+	);
+}
