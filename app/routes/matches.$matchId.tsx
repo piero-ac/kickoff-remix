79a38769c44cb35f1ef4316138e4c36e924b306c
Piero Coronado <pacoronado01@gmail.com>
2024-01-09 12:44:20 -0500

    Create display of team names, fulltime, and halftime scores for completed matches

--- app/routes/matches.$matchId.tsx
+++ app/routes/matches.$matchId.tsx
@@ -11,15 +11,15 @@ export async function loader({ params }: LoaderFunctionArgs) {
 		throw new Response("Missing Match Id", { status: 400 });
 	}
 	const match = await getMatchInfo(matchId);
-	// const lineup = await getMatchLineup(matchId);
-	return json({ match });
+	const lineup = await getMatchLineup(matchId);
+	return json({ match, lineup });
 }
 
 export default function MatchInformation() {
-	const { match } = useLoaderData<typeof loader>();
+	const { match, lineup } = useLoaderData<typeof loader>();
 	const dateLocalTime = convertDateToLocalTime(match.fixture.date);
 	const date = `${dateLocalTime.day} ${dateLocalTime.month} ${dateLocalTime.year}`;
-	// const [home, away] = lineup;
+	const [home, away] = lineup;
 
 	return (
 		<div className="bg-brightwhite h-full p-1">
@@ -51,7 +51,37 @@ export default function MatchInformation() {
 					</div>
 				)}
 			</header>
-			{match.fixture.id}
+			<section className="flex flex-row justify-between mt-3 p-1 border-b border-b-darkpurple">
+				<div className="flex flex-col items-center text-darkpurple font-bold grow">
+					<img
+						src={match.teams.home.logo}
+						alt={`${match.teams.home.name}'s Logo`}
+						width="100px"
+					/>
+
+					<p>{match.teams.home.name}</p>
+				</div>
+				<div className="bg-darkpurple rounded-lg text-white min-w-[200px] flex flex-col items-center justify-center">
+					<p className="text-5xl font-bold">
+						{match.score.fulltime.home} - {match.score.fulltime.away}
+					</p>
+					<p className="">
+						Halftime: {match.score.halftime.home} - {match.score.halftime.away}
+					</p>
+				</div>
+				<div className="flex flex-col items-center text-darkpurple font-bold grow">
+					<img
+						src={match.teams.away.logo}
+						alt={`${match.teams.away.name}'s Logo`}
+						width="100px"
+					/>
+					<p>{match.teams.away.name}</p>
+				</div>
+			</section>
+			<section className="mt-1">
+				<h1>Lineups</h1>
+				{home.coach.name}
+			</section>
 		</div>
 	);
 }
