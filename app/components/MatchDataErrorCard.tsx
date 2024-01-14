78305dd3071cb340e91281c7d35fef5b663071bc
Piero Coronado <pacoronado01@gmail.com>
2024-01-14 17:21:29 -0500

    Refactor styling and message selection based on error

--- app/components/MatchDataErrorCard.tsx
+++ app/components/MatchDataErrorCard.tsx
@@ -1,14 +1,23 @@
 export default function ErrorCard({
 	title,
 	message,
+	statusCode,
 }: {
 	title: string;
 	message: string | null;
+	statusCode?: number;
 }) {
+	let messageByStatus =
+		statusCode === 404
+			? "Please check back later."
+			: statusCode === 500
+			? "Server is down."
+			: message;
+
 	return (
-		<div className="cyan-gradient text-brightwhite min-h-[150px] text-center flex flex-col items-center justify-center">
+		<div className="cyan-gradient text-brightwhite text-center flex flex-col items-center justify-center h-full">
 			<h2 className="text-2xl font-bold  uppercase w-full">{title}</h2>
-			{message && <p>{message}</p>}
+			<p>{messageByStatus}</p>
 		</div>
 	);
 }
